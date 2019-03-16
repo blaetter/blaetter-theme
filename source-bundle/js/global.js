3754da7e2baa87e73448bd5031c192a0d4fff84e
ambo <andreas.maengel@gmail.com>
2019-03-16 16:14:59 +0100

    added search flyout js, removed old js plugins

--- source-bundle/js/global.js
+++ source-bundle/js/global.js
@@ -1,26 +1,16 @@
 
-//import * as datepicker from 'datepicker-init';
-//import * as lightbox from 'lightbox.js';
-//import * as shariff from 'shariff-init';
 import * as swiper from 'swiper-init';
-//import accordion from 'accordion';
-//import articleimage_add_blur from 'articleimage-add-blur.js';
-//import block_limit_height from 'block-limit-height.js';
 import collapse from 'collapse';
 import nav_menu_flyout from 'nav-menu-flyout';
+import search_flyout from 'search-flyout';
 
 window._paq = typeof window._paq === "object" ? window._paq : [];
 window._gaq = typeof window._gaq === "object" ? window._gaq : [];
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
-	//accordion();
-	//articleimage_add_blur();
-	//block_limit_height();
 	collapse();
-	//datepicker.init(datepicker.apply_standard);
-	//lightbox.init(lightbox.apply);
-	//shariff.init(shariff.apply);
 	swiper.init(swiper.apply);
 	nav_menu_flyout();
+	search_flyout();
 });
