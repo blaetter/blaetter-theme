e915f5b819d46dac85b812265cf9601dc23d2b82
ambo <andreas.maengel@gmail.com>
2019-03-16 16:14:11 +0100

    removed toggling of menu sub items

--- source-bundle/js/modules/nav-menu-flyout.js
+++ source-bundle/js/modules/nav-menu-flyout.js
@@ -4,10 +4,10 @@
 import * as BLAETTER from 'blaetter';
 
 export default function nav_menu_flyout() {
-	
+
 	var elHamburger = $(".js-hamburger");
     var elNavigatation = $("#navigation-primary");
-	
+
 	function handleMenu() {
         // Hamburger: Toggle Navigation and Page Background
         elHamburger.click(
@@ -23,23 +23,6 @@ export default function nav_menu_flyout() {
                 return false;
             }
         );
-
-        // toggle navigation sublist items
-        $("#navigation-primary ul.menu > li.menu-item--collapsed > a, #navigation-primary ul.menu > li.menu-item--expanded > a").each(function(){
-            $(this).after('<button class="arrow"><i class="fa fa-chevron-down" aria-hidden="true"></i></button>');
-        });
-        $("#navigation-primary ul.menu > li.menu-item--collapsed .arrow, #navigation-primary ul.menu > li.menu-item--expanded .arrow").click(
-            function(event) {
-                event.preventDefault();
-                $('#navigation-primary ul.menu > li.menu-item--expanded .arrow').not(this).parent().removeClass('menu-item--expanded').addClass('menu-item--collapsed');
-                if ( $(this).parent().hasClass('menu-item--collapsed') ) {
-                    $(this).parent().removeClass('menu-item--collapsed').addClass('menu-item--expanded');
-                } else {
-                    $(this).parent().removeClass('menu-item--expanded').addClass('menu-item--collapsed');
-                }
-            }
-        );
-
 	}
     handleMenu();
 }
