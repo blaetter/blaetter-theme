e0e64fe8fc4bc96884281276ac90aa386c457572
ambo <andreas.maengel@gmail.com>
2019-07-04 22:54:46 +0200

    added scrollbar to header

--- source-bundle/js/modules/header-fix.js
+++ source-bundle/js/modules/header-fix.js
@@ -20,7 +20,21 @@ export default function header_fix() {
             elHeader.removeClass('fixed');
             elContent.removeClass('fixed');
         }
-	}
+    }
     $(window).scroll(header_fix);
     header_fix();
+
+    /**
+     * Sets scrollbar with if available so we can indicate the length of a page,
+     * needs element with id="scrollbar" in the document
+     */
+    function scrollbar() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("scrollbar").style.width = scrolled + "%";
+    }
+    if (null !== document.getElementById("scrollbar")) {
+        window.onscroll = function () { scrollbar() };
+    }
 }
