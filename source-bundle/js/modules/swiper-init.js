4ae717d94f5774661f1a8dc38e9c7a0193947b51
ambo <andreas.maengel@gmail.com>
2019-10-28 19:40:00 +0100

    fixed swiper behaving inconsistently between breakpoints, removed click slide event

--- source-bundle/js/modules/swiper-init.js
+++ source-bundle/js/modules/swiper-init.js
@@ -44,7 +44,7 @@ export function apply(Swiper) {
 	var archiveindexSelector = '.swiper.archive-index';
 	var archiveindexOptions = {
 		grabCursor: true,
-		slideToClickedSlide: true,
+		slideToClickedSlide: false,
 		direction: 'horizontal',
 		slidesPerView: 6,
 		slidesPerGroup: 5,
@@ -64,10 +64,15 @@ export function apply(Swiper) {
 			prevEl: '.swiper-button-prev',
 		},
 		breakpoints: {
-			1000: {
-				// palm
+			1200: {
+				// bigger
 				slidesPerView: 3,
 				slidesPerGroup: 3,
+			},
+			720: {
+				// palm
+				slidesPerView: 2,
+				slidesPerGroup: 2,
 			},
 			420: {
 				// smallest
@@ -77,120 +82,4 @@ export function apply(Swiper) {
 		}
 	};
 	new Swiper(archiveindexSelector, archiveindexOptions);
-
-
-	// SWIPER: galleryselect
-	/*
-	var galleryselectSelector = '.swiper-galleryselect .swiper';
-	var galleryselectOptions = {
-		grabCursor: true,
-		slideToClickedSlide: true,
-		direction: 'horizontal',
-		slidesPerView: 3,
-		slidesPerGroup: 3,
-		spaceBetween: 24,
-		loop: true,
-		preloadImages: false,
-		touchReleaseOnEdges: true,
-		touchEventsTarget: 'wrapper',  // do not touch our navigation buttons
-		watchOverflow: true,
-		lazy: {
-			loadPrevNextAmount: 3
-		},
-		pagination: {
-			el: '.js-galleryselect .swiper-pagination',
-			type: 'fraction',
-		},
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			720: {
-				// palm
-				slidesPerView: 2.4,
-				slidesPerGroup: 2,
-			},
-			420: {
-				// smallest
-				slidesPerView: 1.3,
-				slidesPerGroup: 1,
-			},
-		}
-	};
-	new Swiper(galleryselectSelector, galleryselectOptions);
-	*/
-
-	// SWIPER: Article Mainimage
-	/*
-	var galleryArticleimageSelector = '.swiper-articleimage .swiper';
-	var galleryArticleimageOptions = {
-		grabCursor: true,
-		zoom: false, // not good in mobile, only doubleclick
-		longSwipesMs: 400,
-		threshold: 10, // Threshold value in px
-		slideToClickedSlide: false,
-		direction: 'horizontal',
-		loop: true,
-		slidesPerView: 1,
-		slidesPerGroup: 1,
-		autoHeight: true,
-		spaceBetween: 24,
-		touchReleaseOnEdges: true,
-		touchEventsTarget: 'wrapper', // do not touch our navigation buttons
-		hashNavigation: {
-			watchState: false,
-			replaceState: false,
-		},
-		pagination: {
-			el: '.swiper-articleimage .swiper-pagination',
-			type: 'custom',
-			renderCustom: function (swiper, current, total) {
-				return 'Bild ' + current + ' von ' + total;
-			},
-		},
-		navigation: {
-			nextEl: '.swiper-articleimage .swiper-button-next',
-			prevEl: '.swiper-articleimage .swiper-button-prev',
-		},
-		// lazy loading images
-		preloadImages: false,
-		lazy: {
-			loadPrevNext: true,
-			loadPrevNextAmount: 1,
-			loadOnTransitionStart: true,
-		},
-		on: {
-			lazyImageReady: function (swiper, slide) {
-				// Duplicate and BLUR the backgroundcopy of an image to fill out
-				// different image ratios
-				// Info: CSS Bluring is very unperformant on mobile devices!
-				// Zoom inside a blured gallery can crash the browser.
-				// The "transform" property force the user’s browser to hardware
-				// accelerate,
-				// if you try blurring an element without it applied, you’ll see
-				// a general drop in frame rate!
-				$(slide).parent().css({
-					'position': 'relative',
-					'overflow': 'hidden'
-				});
-				$(slide).css('position', 'relative');
-				$(slide).clone().prependTo($(slide).parent()).removeClass()
-					.addClass('image-blured').css({
-						'position': 'absolute',
-						'width': '100%',
-						'height': '100%',
-						'max-width': 'none',
-						'left': '0',
-						'top': '6%',
-						'filter': 'blur(12px)',
-						'transform': 'translate3d(0, 0, 0)',
-						'transform': 'scale(1.2)'
-					});
-			}
-		}
-	};
-	new Swiper(galleryArticleimageSelector, galleryArticleimageOptions);
-	*/
-
 }
