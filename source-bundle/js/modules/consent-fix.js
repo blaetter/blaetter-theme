6ccfe344e150238a71aa0ee046a9c73175337988
ambo <andreas.maengel@gmail.com>
2019-12-22 12:01:10 +0100

    updated consent styles and js

--- source-bundle/js/modules/consent-fix.js
+++ source-bundle/js/modules/consent-fix.js
@@ -4,19 +4,31 @@
 
 export default function consent_fix() {
 
+  if (typeof(Drupal) !== "undefined") {
     var elPopup = $('#sliding-popup');
     var bottom_navigation = $('#block-fussbereich > .menu');
     // only show consent by default if user did not choose anything before.
     if (null === Drupal.eu_cookie_compliance.getCurrentStatus()) {
       if (null !== elPopup) {
         elPopup.addClass('visible');
+        elPopup.addClass('initial');
       }
     }
     elPopup.find('.button').each(function() {
       jQuery(this).click(function() {
         $('#sliding-popup').removeClass('visible');
+        $('#sliding-popup').removeClass('initial');
       });
     });
+
+    if (elPopup.hasClass('initial')) {
+      var settings_button = $('<button class="button button--reversed">Cookie-Einstellungen</button>');
+      settings_button.click(function() {
+        $('#sliding-popup').removeClass('initial');
+        $(this).remove();
+      });
+      $('#popup-buttons').append(settings_button);
+    }
 
     var privacy_link = $('<a href="#">Datenschutz-Einstellungen</a>');
     privacy_link.click(function() {
@@ -26,4 +38,5 @@ export default function consent_fix() {
     privacy_list.append(privacy_link);
 
     bottom_navigation.append(privacy_list);
+  }
 }
