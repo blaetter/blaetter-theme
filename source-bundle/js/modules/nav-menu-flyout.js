08e0f5db3e1651600f4c642c08ed60eff6732e38
ambo <andreas.maengel@gmail.com>
2020-01-04 20:10:54 +0100

    added support for esc key and body click to close menu

--- source-bundle/js/modules/nav-menu-flyout.js
+++ source-bundle/js/modules/nav-menu-flyout.js
@@ -6,23 +6,40 @@ import * as BLAETTER from 'blaetter';
 export default function nav_menu_flyout() {
 
 	var elHamburger = $(".js-hamburger");
-    var elNavigatation = $("#navigation-primary");
+  var elNavigation = $("#navigation-primary");
 
 	function handleMenu() {
+
         // Hamburger: Toggle Navigation and Page Background
         elHamburger.click(
             function(event) {
                 event.preventDefault();
                 if (! $(this).hasClass('opened')) {
+                    // add open class to menu link
                     $(this).addClass('opened').attr('aria-label','Navigation zuklappen');
-                    elNavigatation.addClass('opened');
-                }else{
+                    // open navigation itself
+                    elNavigation.addClass('opened');
+                    // add close method on body click
+                    $('body').click(function(click_event) {
+                        if(0 == $(click_event.target).parents('#navigation-primary').length && elHamburger.hasClass('opened')) {
+                            elHamburger.click();
+                        }
+                    });
+                    // add close method on esc key up
+                    var KEYCODE_ESC = 27;
+                    $(document).keyup(function(e) {
+                        if (e.keyCode == KEYCODE_ESC && elHamburger.hasClass('opened')) {
+                            elHamburger.click();
+                        }
+                    });
+                } else {
                     $(this).removeClass('opened').attr('aria-label','Navigation aufklappen');
-                    elNavigatation.removeClass('opened');
+                    elNavigation.removeClass('opened');
                 }
                 return false;
             }
         );
-	}
-    handleMenu();
+  }
+
+  handleMenu();
 }
