3bc2d6fe3b5d06ca8ff0757394ebb287b1dd021e
ambo <andreas.maengel@gmail.com>
2021-06-30 20:23:59 +0200

    change search form action based on visibility of the search form.
    This is to prevent scrapers from submitting the form when it is invisible so the bing search is not queried by bots.

--- source-bundle/js/modules/search-flyout.js
+++ source-bundle/js/modules/search-flyout.js
@@ -6,7 +6,8 @@ import * as BLAETTER from 'blaetter';
 export default function search_flyout() {
 
 	var elSearchButton = $(".js-search");
-    var elSearch = $("#page-search");
+  var elSearch = $("#page-search");
+  var elSearchForm = $(".form--navigationsearch");
 
 	function handleSearch() {
         elSearchButton.click(
@@ -14,10 +15,12 @@ export default function search_flyout() {
                 event.preventDefault();
                 if (! $(this).hasClass('opened')) {
                     $(this).addClass('opened').attr('aria-label','Suche zuklappen');
+                    elSearchForm.attr('action', '/search/bing');
                     elSearch.addClass('opened');
                 }else{
                     $(this).removeClass('opened').attr('aria-label','Suche aufklappen');
                     elSearch.removeClass('opened');
+                    elSearchForm.attr('action', '#');
                 }
                 return false;
             }
